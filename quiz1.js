0f04eeaec5df2fa366ca9597ff48a8db885b7f1d
SupremeNoble <Syed9098@gmail.com>
2023-04-02 14:31:04 +0800

    classwork update

---
+++ quiz1.js
@@ -0,0 +1,52 @@
+//let message = "benr"
+//console.log(message)
+//console.log(message, typeof message)
+
+//message = 123
+//console.log(message, typeof message)
+
+//let str = "BENR"
+//let num = 3523
+//let bool = false
+
+//let course = str + bool
+//console.log(course, typeof course)
+
+//let str = "1.258"
+//let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total,typeof total)
+
+/* let data = ["benr",3523,false]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    nama:"syed",
+    umur: 24,
+    kerja: false
+}
+
+console.log(user.nama)
+console.log(user.umur)
+console.log(user.kerja) */
+
+let messages = [
+    {
+        name: "user0",
+        date: "30/3/2023",
+        msg: "tak comel",
+        like: 86
+    },
+    {
+        name: "user1",
+        date: "29/3/2023",
+        msg: "comel",
+        like: 68
+    }
+]
+console.log(messages[0].msg)
